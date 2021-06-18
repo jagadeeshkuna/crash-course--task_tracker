227032dc1eef09e35bff8cec22cc59e6df9ed7bf
500apps-jagadeeshkuna <85148614+jagadeeshkuna@users.noreply.github.com>
2021-06-18 18:19:25 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,68 @@
+<template>
+<div class="container">
+<Header  @toggle-add-task="toggleAddTask"
+ title="TASK TRACKER" :showAddTask="showAddTask" />
+
+
+<router-view :showAddTask="showAddTask"></router-view>
+<Footer/>
+</div>
+</template>
+<script>
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer
+    
+},
+data(){
+  return{
+
+    showAddTask: false,
+  }
+},
+methods: {
+  toggleAddTask(){
+    this.showAddTask = !this.showAddTask
+  },
+},
+}
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=popins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'poppins', sans-serif;
+}
+.container {
+  max-width:500px;
+  margin: 30px auto;
+  overflow:auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding:30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size:15px;
+  font-family: inherit;
+}
+</style>
